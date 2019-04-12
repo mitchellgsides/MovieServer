3d51b629607169013ce0c1a78501afa93407d2f1
mitchellgsides <mitchellgsides@gmail.com>
2019-04-11 19:13:44 -0500

    fixed app.use bug

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(morgan(morganSettings));
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-  const authToken = req.get("Authorization").split(" ")[2];
+  const authToken = req.get("Authorization");
   const apiToken = process.env.API_TOKEN;
 
-  if (!authToken || authToken !== apiToken) {
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
   // move to the next middleware
